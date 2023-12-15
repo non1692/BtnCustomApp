ec8d6941a7cafa9cae0f259b07d51abf6baf394f
noerojasf <146777762+noerojasf@users.noreply.github.com>
2023-12-14 18:09:18 -0600

    update versión

--- app/buttoncustom/build.gradle.kts
+++ app/buttoncustom/build.gradle.kts
@@ -38,7 +38,7 @@ publishing{
         register<MavenPublication>("release") {
             groupId = "com.github.non1692"
             artifactId = "BtnCustomApp"
-            version = "1.0.0"
+            version = "1.0.1"
 
             afterEvaluate {
                 from(components["release"])
